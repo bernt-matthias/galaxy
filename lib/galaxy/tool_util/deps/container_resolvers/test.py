894cdece7af5810bfd396c4469a0662370c624b2
Matthias Bernt <m.bernt@ufz.de>
2023-04-05 22:49:10 +0200

    add null container resolver

---
+++ lib/galaxy/tool_util/deps/container_resolvers/test.py
@@ -0,0 +1,31 @@
+from typing import (
+    List,
+    Optional,
+    TYPE_CHECKING,
+)
+
+from . import ContainerResolver
+
+if TYPE_CHECKING:
+    from ..dependencies import (
+        ToolInfo,
+    )
+    from ..requirements import ContainerDescription
+
+
+class NullContainerResolver(ContainerResolver):
+    """
+    helper ContainerResolver that does not resolve ever
+
+    for testing
+    """
+
+    resolver_type = "null"
+
+    def resolve(
+        self, enabled_container_types: List[str], tool_info: "ToolInfo", **kwds
+    ) -> Optional["ContainerDescription"]:
+        return None
+
+
+__all__ = ("NullContainerResolver",)
